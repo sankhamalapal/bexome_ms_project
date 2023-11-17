2d1dcb3d6452f6db8241759d273c3e5ab18e0586
sankhamalapal <sankhamala13@gmail.com>
2023-11-17 11:11:39 +0100

    Updating the Project folder

--- bexome_flutter_ss2023_v_3_10_5_1/android/app/src/main/kotlin/com/example/bexome_flutter_ss2023_v_3_10_5_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bexome_flutter_ss2023_v_3_10_5_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- bexome_flutter_ss2023_v_3_10_5_1/lib/Translation/translation.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'demo_localization.dart';
-
-class Translation {
-  String getTranslation(BuildContext context, String key) {
-    return DemoLocalizations.of(context).getTranslatedValues(key);
-  }
-}
